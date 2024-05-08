2f2eccf8cf1d663f8db6b416425e524651465e10
LauraDev <lauralodeirodev@gmail.com>
2024-05-08 13:50:41 +0200

    #9

--- Nivel facil/reto9/Principal.java
+++ Nivel facil/reto9/Principal.java
@@ -7,6 +7,30 @@ package reto9;
 
 public class Principal {
 public static void main(String[] args) {
+    int decimal=0;
+    deciamlTobinario(decimal);
+}
+public static void deciamlTobinario(int decimal){
+    StringBuilder binario=new StringBuilder();
+
+    while (decimal>0) {
+        int digito=decimal%2;
+        decimal/=2;
+        if (digito==0) {
+            binario.append(digito);
+            
+        }else{
+            binario.append(1);
+        }
+    }
+
+    binario.reverse();
+
+    if (binario.length()==0) {
+        System.out.println(0);
+    }else{
+        System.out.println(binario.toString());
+    }
     
 }
 }
