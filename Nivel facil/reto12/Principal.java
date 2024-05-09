93a9081701ffb509203ef21834b40b532d4df436
LauraDev <lauralodeirodev@gmail.com>
2024-05-09 10:05:23 +0200

    #12

---
+++ Nivel facil/reto12/Principal.java
@@ -0,0 +1,55 @@
+
+
+/*
+ * Crea una función que reciba dos cadenas como parámetro (str1, str2)
+ * e imprima otras dos cadenas como salida (out1, out2).
+ * - out1 contendrá todos los caracteres presentes en la str1 pero NO
+ *   estén presentes en str2.
+ * - out2 contendrá todos los caracteres presentes en la str2 pero NO
+ *   estén presentes en str1.
+ */
+
+ package reto12;
+ 
+ class Principal {
+ 
+    public static void main(String[] args) {
+        String str1="Casa";
+        String str2="coChe";
+
+        convert(str1,str2);
+
+
+    }
+
+    public static void convert(String str1, String str2){
+
+        char[]palabra1=str1.toLowerCase().toCharArray();
+        char[]palabra2=str2.toLowerCase().toCharArray();
+
+        StringBuilder out1=new StringBuilder();
+        StringBuilder out2=new StringBuilder();
+
+        for(int i=0; i<str1.length();i++){
+
+            if (str2.indexOf(str1.charAt(i))==-1) {
+                if (out1.toString().contains(String.valueOf(str1.charAt(i)))==false) {
+                    
+                 out1.append(str1.charAt(i));   
+                }
+                
+            }
+
+
+        }
+
+        System.out.println(out1.toString());
+
+    }
+
+    
+
+    }
+    
+
+    
